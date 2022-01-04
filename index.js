d2018013e04cd104234d29e0846cfa15559d4ef0
Mikita Papok <73660686+mikitapapok@users.noreply.github.com>
2022-01-04 18:22:18 +0300

    add task 1

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.set('view engine', 'pug');
 
+app.get('/api/greetings/:name', (req, res) => {
+    res.render('index.pug', { name: req.params.name });
+});
+
+app.listen(port, () => {
+    console.log('Это короче, вот те сервер. Понял, да?');
+});
