92712fd9ada1df48a4b51e1f266448012ebb6096
Mikita Papok <73660686+mikitapapok@users.noreply.github.com>
2022-01-04 18:52:22 +0300

    format files

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 app.set('view engine', 'pug');
 
 app.get('/api/greetings/:name', (req, res) => {
     res.render('index.pug', { name: req.params.name });
 });
 
-app.listen(port, () => {
-    console.log('Это короче, вот те сервер. Понял, да?');
+app.listen(PORT, () => {
+    console.log(PORT);
 });
