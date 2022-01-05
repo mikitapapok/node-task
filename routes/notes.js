5ec8d90191f01fc4689435d68ca53f6796aac9c2
Mikita Papok <73660686+mikitapapok@users.noreply.github.com>
2022-01-05 18:00:38 +0300

    add POST

--- routes/notes.js
+++ routes/notes.js
@@ -2,9 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-
 router.get('/notes', (req, res) => {
     res.send('notes');
+});
+
+router.post('/notes', (req, res) => {
+    res.send(req.body.json());
 });
 
 module.exports = router;
